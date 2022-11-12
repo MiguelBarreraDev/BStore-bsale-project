b7dc27ffceb6f808a05e6bbd7434b9bb6bd8435b
MiguelBarreraDev <barreram987@gmail.com>
2022-11-12 04:49:48 -0500

    Add: suport Async calls

--- frontend/src/lib/remi.js
+++ frontend/src/lib/remi.js
@@ -13,16 +13,21 @@ export const toElement = (type, { children, ...props }) => {
   if (typeof children === 'string')
     element.appendChild(document.createTextNode(children))
   else if (children instanceof Array)
-    children.forEach(elm => element.appendChild(elm))
+    children.forEach(elm => {
+      if (typeof elm === 'string')
+        element.appendChild(document.createTextNode(elm))
+      else
+        element.appendChild(elm)
+    })
   else if (children)
     element.appendChild(children)
 
   return element
 }
 
-export const renderElement = (root, element) => {
-  if (element)
-    root.appendChild(element)
+export const renderElement = async (root, component, mode = 'sync') => {
+  if (mode === 'sync') root.replaceChildren(component())
+  if (mode === 'async') root.replaceChildren(await component())
 }
 
 export const _xjs = toElement
