9fb664e20dd2315e33d947541dfb722e43a1fb0c
MiguelBarreraDev <barreram987@gmail.com>
2022-11-12 04:53:28 -0500

    Feat: Endpoint to search products

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -1,11 +1,13 @@
 import productController from '#controllers/products/product.controller.js'
 import productsAllController from '#controllers/products/products-all.controller.js'
+import productsSearchController from '#controllers/products/products-search.controller.js'
 import { Router } from 'express'
 
 const productsRouter = Router()
 
 productsRouter
   .get('/', productsAllController)
+  .get('/search', productsSearchController)
   .get('/:id', productController)
 
 export default productsRouter
