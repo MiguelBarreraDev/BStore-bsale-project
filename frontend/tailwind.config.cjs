d750651c5b3b139b04fb40d06641ef129dccd1a0
MiguelBarreraDev <barreram987@gmail.com>
2022-11-12 04:49:19 -0500

    Add: keyframes

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fadeout': {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0, display: 'hidden' }
+        }, 
+        'fadein': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        } 
+      }
+    },
   },
   plugins: [],
 }
