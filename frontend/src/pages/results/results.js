b344366172f867dfe9386e2e192ba77fa21e93c7
MiguelBarreraDev <barreram987@gmail.com>
2022-11-13 16:18:02 -0500

    Refactor: Extract code in services

--- frontend/src/pages/results/results.js
+++ frontend/src/pages/results/results.js
@@ -1,22 +1,21 @@
 import '@/css/style.css'
-import { _xjs, renderElement } from '@/lib/remi'
+import { _xjs } from '@/lib/remi'
 import { renderCategoriesNav, renderProducts } from '@/components'
 import { $, searchFuncionality } from '@/utils'
-import {searchProductsService} from '@/services'
+import { searchProductsService } from '@/services'
 
 /*----------------------*/
 /* Built in
-  /*----------------------*/
+/*----------------------*/
 renderCategoriesNav()
-
 
 /*----------------------*/
 /* Custom
 /*----------------------*/
 const paramsFromUrl = new URLSearchParams(window.location.search)
-const search_query = paramsFromUrl.get('search_query')
+const searchQuery = paramsFromUrl.get('search_query')
 
-searchFuncionality({ defaultValue: search_query })
+searchFuncionality({ defaultValue: searchQuery })
 
 const getFilteredProducts = async () => {
   const searchProductInput = $('#search-product-input')
