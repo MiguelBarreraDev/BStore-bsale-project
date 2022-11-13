d6650d81612059be8b7f3673785f69476f15955f
MiguelBarreraDev <barreram987@gmail.com>
2022-11-12 21:08:08 -0500

    pages/category/category.js

---
+++ frontend/src/main.js
@@ -0,0 +1,14 @@
+import '@/css/style.css'
+import { _xjs } from '@/lib/remi'
+import { renderCategoriesNav, renderProducts } from './components'
+import { searchFuncionality } from './utils'
+
+/*--------------------------------*/
+/* Built in
+/*--------------------------------*/
+renderCategoriesNav()
+
+searchFuncionality()
+
+/* Custom */
+renderProducts()
