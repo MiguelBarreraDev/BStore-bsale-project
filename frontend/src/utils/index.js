8896bdec707c8a38b5dabdea393fc805220c4da6
MiguelBarreraDev <barreram987@gmail.com>
2022-11-13 16:21:07 -0500

    Update: barrel

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,3 +1,4 @@
 export * from './global-object'
 export { default as searchFuncionality } from './search-funcionality'
 export { default as responsiveNavFuncionality } from './responsive-nav-funcionality'
+export { default as productsViewtFuncionality } from './products-view-funcionality'
