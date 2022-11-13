391fe3f8ab2fc75a254e580b814fdd27518fe2fe
MiguelBarreraDev <barreram987@gmail.com>
2022-11-13 16:20:10 -0500

    Feat: Read and write from local storage with parser for json

--- frontend/src/utils/global-object.js
+++ frontend/src/utils/global-object.js
@@ -1,2 +1,6 @@
 export const $ = selector => document.querySelector(selector)
 export const ls = window.localStorage
+export const jsonLs = {
+  read: (key) => JSON.parse(ls.getItem(key)),
+  write: (key, value) => ls.setItem(key, JSON.parse(value))
+}
