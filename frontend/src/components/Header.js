4829bf59131151ac7caf6feac9175458c110c6cc
MiguelBarreraDev <barreram987@gmail.com>
2022-11-13 02:19:07 -0500

    Feat: Show active link in categories nav

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,33 @@
 import { renderElement, _xjs } from "@/lib/remi"
 import { categoriesService } from "@/services"
-import { $, responsiveNavFuncionality } from "@/utils"
+import { $, ls, responsiveNavFuncionality } from "@/utils"
 
-const Category = ({ name, id }) => (
-  _xjs('li', {
-    children: _xjs('a', {
-      class: 'truncate capitalize transition md:hover:text-orange-400 text-slate-500 font-serif md:font-sans text-3xl md:text-lg font-semibold',
-      href: `/pages/category/category.html?id=${id}&name=${name}`,
-      children: name
+const Category = ({ name, id }) => {
+  const paramsFromUrl = new URLSearchParams(window.location.search)
+  const active = paramsFromUrl.get('id') == id
+
+  return (
+    _xjs('li', {
+      children: _xjs('a', {
+        class: `truncate capitalize transition md:hover:text-orange-300 ${active ? 'text-orange-400': 'text-slate-500'} font-serif md:font-sans text-3xl md:text-lg font-semibold`,
+        href: `/pages/category/category.html?id=${id}&name=${name}`,
+        children: name
+      })
     })
-  })
-)
+  )
+}
 
 const CategoriesNav = async () => {
-  const { data: categoriesList } = await categoriesService()
+  let cachedData =  JSON.parse(ls.getItem('categories'))
+  let categoriesList
+
+  if (cachedData) {
+    categoriesList = cachedData
+  } else {
+    const { data } = await categoriesService()
+    categoriesList = data
+    ls.setItem('categories', JSON.stringify(categoriesList))
+  }
 
   responsiveNavFuncionality()
 
