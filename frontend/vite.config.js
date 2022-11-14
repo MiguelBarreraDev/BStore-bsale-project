d35ea89a7d52f768413c034cfca879e79ea533f7
MiguelBarreraDev <barreram987@gmail.com>
2022-11-13 23:00:07 -0500

    Update: envDir path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,6 @@ import path from 'path'
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
   build: { outDir: '../dist' },
+  envDir: '../',
   resolve: { alias: { '@': path.resolve(__dirname, './src') } }
 })
