238e6325fb4be8b64389b69724d8608c1ec2c1c6
MiguelBarreraDev <barreram987@gmail.com>
2022-11-13 23:54:04 -0500

    Update: Vite config for routes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 
+const root = path.resolve(__dirname, 'src')
+const outDir = path.resolve(__dirname, 'dist')
+
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
-  build: { outDir: '../dist' },
+  root,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(root, 'index.html'),
+        category: path.resolve(root, 'pages', 'category', 'category.html'),
+        results: path.resolve(root, 'pages', 'results', 'results.html')
+      }
+    }
+  },
   resolve: { alias: { '@': path.resolve(__dirname, './src') } }
 })
